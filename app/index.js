5a52a7be969571494de0803e16e2355e57b801d1
Sarthak <sarthak.srivastava@groww.in>
2022-04-15 15:48:32 +0530

    feat(add): webpack

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+import React from 'react';
+
+export class app extends React.Component {
+
+    render () {
+    return (
+        <div>ascasc</div>
+    )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+   document.getElementById('app')
+  )
